ac61b8dfbf41ff0f1fc4242c9757754c64025d8f
Ostap Kovbasiuk <ostapkovbasyk@gmail.com>
2020-02-19 23:44:37 +0200

    Functions presentation part 2

--- Functions/examples/file.js
+++ Functions/examples/file.js
@@ -78,4 +78,95 @@ console.log(num); // undefined
 var num;
 num = 6;
 
+function exampleFunction(a, b, c) {
+  console.log(arguments[0]);
+  console.log(arguments[1]);
+  console.log(arguments[2]);
+}
+exampleFunction(1, 2, 3); // 1 2 3
+
+let args = Array.from(arguments);
+
+let args = [...arguments];
+
+var obj = { name: 'Niladri' };
+
+var greeting = function(a, b, c) {
+  return 'Welcome ' + this.name + ' to ' + a + ' ' + b + ' in ' + c;
+};
+
+console.log(greeting.apply(obj, ['Newtown', 'KOLKATA', 'WB']));
+// Welcome Niladri to Newtown KOLKATA in WB
+
+var obj = { name: 'Niladri' };
+
+var greeting = function(a, b, c) {
+  return 'Welcome ' + this.name + ' to ' + a + ' ' + b + ' in ' + c;
+};
+
+//creates a bound function that has same body and parameters
+var bound = greeting.bind(obj);
+
+console.log(bound('Newtown', 'KOLKATA', 'WB')); //call the bound function
+
+// Welcome Niladri to Newtown KOLKATA in WB
+
+(function nameCanBeHereButNoReason() {
+  console.log('HelloWorld');
+})(); // HelloWorld
+
+(function() {
+  var aName = 'Barry';
+})();
+// Variable aName is not accessible from the outside scope
+console.log(aName); // throws "Uncaught ReferenceError: aName is not defined"
+
+var result = (() => {
+  var name = 'Barry';
+  return name;
+})();
+
+console.log(result); // "Barry"
+
+var name = 'Jerry';
+console.log(name); // "Jerry"
+(() => {
+  this.name = 'Barry';
+})();
+
+console.log(name); // "Barry"
+
+function makeFunc() {
+  var name = 'Chrome';
+  function displayName() {
+    console.log(name);
+  }
+  return displayName;
+}
+
+var myFunc = makeFunc();
+myFunc(); // Chrome
+
+function makeAdder(x) {
+  return function(y) {
+    return x + y;
+  };
+}
+
+var add5 = makeAdder(5);
+var add10 = makeAdder(10);
+
+console.log(add5(2)); // 7
+console.log(add10(2)); // 12
+
+function makeSizer(size) {
+  return function() {
+    document.body.style.fontSize = size + 'px';
+  };
+}
+
+var size12 = makeSizer(12);
+var size14 = makeSizer(14);
+var size16 = makeSizer(16);
+
 asd;
